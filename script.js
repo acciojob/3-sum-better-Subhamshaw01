cee7cf8fa3921dc94a5257ab9241f1c2c0b7f0b4
Subhamshaw01 <137093444+Subhamshaw01@users.noreply.github.com>
2023-09-18 00:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function threeSum(arr, target) {
+	let r=[];
+	for(let i=0;i<arr.length-2;i++)
+		{
+			for(let j=0;j<arr.length-1;j++)
+				{
+					for(let k=0;k<arr.length;k++)
+						{
+							if(r[i]+r[j]+r[k]==target)
+							{
+								document.write(r[i]+" "+r[j]+" "+r[k])
+								return true;
+							}
+						}
+				}
+			
+		}
+	return false;
 // write your code here
   
 }
